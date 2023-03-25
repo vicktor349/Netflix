88de35307955db54387afc1100663a0687127c0d
vicktor349 <ayomidevictor349@gmail.com>
2023-03-25 13:57:12 +0100

    Made Changes

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -18,11 +18,6 @@ const Auth = () => {
 
     return (
         <>
-            <Head>
-                <title>
-                    Netflix | {variant}
-                </title>
-            </Head>
             <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
                 <div className="bg-black w-full h-full bg-opacity-50">
                     <nav className="px-12 py-5">
